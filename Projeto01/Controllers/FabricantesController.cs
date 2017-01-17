28adf1b212d4eaee7249ea7fd4f9102115e2d276
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-01-17 09:50:51 -0200

    Add project files.

---
+++ Projeto01/Controllers/FabricantesController.cs
@@ -0,0 +1,33 @@
+﻿using Projeto01.Contexts;
+using Projeto01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto01.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
